23f951ec34176da4be081ae63204fe9114e13b17
Arvind Bright <arvind.bright100@gmail.com>
2019-04-17 14:48:11 -0700

    Search and wishlist tab implemented

---
+++ app/src/main/java/com/bright/productsearch/WishlistTabFragment.java
@@ -0,0 +1,21 @@
+package com.bright.productsearch;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WishlistTabFragment extends Fragment {
+    private static final String TAG = "WishlistTab";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.wishlist_fragment, container, false);
+
+        return view;
+    }
+}
